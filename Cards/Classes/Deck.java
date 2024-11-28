0f7f53ec9d9605888bb4ed811ac1c95bf536c588
Ferny <fernandoborquez215@gmail.com>
2024-11-27 21:46:22 -0700

    Assets de Python y Csharp

--- Cards/Classes/Deck.java
+++ Cards/Classes/Deck.java
@@ -45,7 +45,7 @@ public class Deck
     private ArrayList<NumberCard> pythonNumberCards;
     // --- Python power cards
     private ArrayList<PowerCard> pythonPowerCards;
-
+    
     // ---------------------------------------------
 
 
@@ -54,63 +54,63 @@ public class Deck
         initializeDeck();
     }
 
-    public void initializeDeck()
+    private void initializeDeck()
     {
         // C++
         // --- C++ number cards
         for(int i = 0; i < 10; i++)
         {
-            cppNumberCards.add(new NumberCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/cpp_" + i + "_card.png", i));
+            cppNumberCards.add(new NumberCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/numberCards/cpp_" + i + "_card.png", i));
         }
         // --- C++ power cards
         // - C++ base card
-        cppPowerCards.add(new PowerCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/cpp_base_card.png", PowerOptions.BASECARD));
+        cppPowerCards.add(new PowerCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/powerCards/cpp_base_card.png", PowerOptions.BASECARD));
         // - C++ take two card
-        cppPowerCards.add(new PowerCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/cpp_taketwo_card.png", PowerOptions.TAKETWO));
+        cppPowerCards.add(new PowerCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/powerCards/cpp_block_card.png", PowerOptions.TAKETWO));
         // - C++ block card
-        cppPowerCards.add(new PowerCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/cpp_block_card.png", PowerOptions.BLOCK));
+        cppPowerCards.add(new PowerCard(ProgrammingLanguage.CPPLAN, "../Assets/cpp/powerCards/cpp_taketwo_card.png", PowerOptions.BLOCK));
 
         // C#
         // --- C# number cards
         for(int i = 0; i < 10; i++)
         {
-            csharpNumberCards.add(new NumberCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/csharp_" + i + "_card.png", i));
+            csharpNumberCards.add(new NumberCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/numberCards/csharp_" + i + "_card.png", i));
         }
         // --- C# power cards
         // - C# base card
-        csharpPowerCards.add(new PowerCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/csharp_base_card.png", PowerOptions.BASECARD));
+        csharpPowerCards.add(new PowerCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/powerCards/csharp_base_card.png", PowerOptions.BASECARD));
         // - C# take two card
-        csharpPowerCards.add(new PowerCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/csharp_taketwo_card.png", PowerOptions.TAKETWO));
+        csharpPowerCards.add(new PowerCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/powerCards/csharp_block_card.png", PowerOptions.TAKETWO));
         // - C# block card
-        csharpPowerCards.add(new PowerCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/csharp_block_card.png", PowerOptions.BLOCK));
+        csharpPowerCards.add(new PowerCard(ProgrammingLanguage.CSHARPLAN, "../Assets/csharp/powerCards/csharp_taketwo_card.png", PowerOptions.BLOCK));
 
         // Java
         // --- Java number cards
         for(int i = 0; i < 10; i++)
         {
-            javaNumberCards.add(new NumberCard(ProgrammingLanguage.JAVALAN, "../Assets/java/java_" + i + "_card.png", i));
+            javaNumberCards.add(new NumberCard(ProgrammingLanguage.JAVALAN, "../Assets/java/numberCards/java_" + i + "_card.png", i));
         }
         // --- Java power cards
         // - Java base card
-        javaPowerCards.add(new PowerCard(ProgrammingLanguage.JAVALAN, "../Assets/java/java_base_card.png", PowerOptions.BASECARD));
+        javaPowerCards.add(new PowerCard(ProgrammingLanguage.JAVALAN, "../Assets/java/powerCards/java_base_card.png", PowerOptions.BASECARD));
         // - Java take two card
-        javaPowerCards.add(new PowerCard(ProgrammingLanguage.JAVALAN, "../Assets/java/java_taketwo_card.png", PowerOptions.TAKETWO));
+        javaPowerCards.add(new PowerCard(ProgrammingLanguage.JAVALAN, "../Assets/java/powerCards/java_block_card.png", PowerOptions.TAKETWO));
         // - Java block card
-        javaPowerCards.add(new PowerCard(ProgrammingLanguage.JAVALAN, "../Assets/java/java_block_card.png", PowerOptions.BLOCK));
+        javaPowerCards.add(new PowerCard(ProgrammingLanguage.JAVALAN, "../Assets/java/powerCards/java_taketwo_card.png", PowerOptions.BLOCK));
 
         // Python
         // --- Python number cards
         for(int i = 0; i < 10; i++)
         {
-            pythonNumberCards.add(new NumberCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/python_" + i + "_card.png", i));
+            pythonNumberCards.add(new NumberCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/numberCards/python_" + i + "_card.png", i));
         }
         // --- Python power cards
         // - Python base card
-        pythonPowerCards.add(new PowerCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/python_base_card.png", PowerOptions.BASECARD));
+        pythonPowerCards.add(new PowerCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/powerCards/python_base_card.png", PowerOptions.BASECARD));
         // - Python take two card
-        pythonPowerCards.add(new PowerCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/python_taketwo_card.png", PowerOptions.TAKETWO));
+        pythonPowerCards.add(new PowerCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/powerCards/python_block_card.png", PowerOptions.TAKETWO));
         // - C++ block card
-        pythonPowerCards.add(new PowerCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/python_block_card.png", PowerOptions.BLOCK));
+        pythonPowerCards.add(new PowerCard(ProgrammingLanguage.PYTHONLAN, "../Assets/python/powerCards/python_taketwo_card.png", PowerOptions.BLOCK));
 
         // Deck
         deck.add(cppDeck);
@@ -118,4 +118,9 @@ public class Deck
         deck.add(javaDeck);
         deck.add(pythonDeck);
     }
-}
+
+    public ArrayList<ArrayList<ArrayList<Card>>> getDeck()
+    {
+        return deck;
+    }
+} 
--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+
+import Cards.*;
+import Cards.Classes.Deck;
 
 public class Main
 {
+    Deck deck = new Deck();
+
     public static void main(String[] args)
     {
-    
+        
     }
 }
 
